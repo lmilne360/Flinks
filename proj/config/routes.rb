f9ac010401bb3a822f82389536394e8d54aa59b9
Laurent <lmilne360@gmail.com>
2017-01-24 18:23:49 -0500

    Added root page for user signins/signup

--- proj/config/routes.rb
+++ proj/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:sessions]
+  root 'welcome#main'
+  	as :user do 
+  		get 'login' => 'devise/sessions#new', as: :new_user_session
+  		post 'login' => 'devise/sessions#create', as: :user_session
+  		get 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  	end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

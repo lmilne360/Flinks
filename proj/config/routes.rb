a925db464cec846e41c88c0246dea233a829c22d
Laurent <lmilne360@gmail.com>
2017-01-24 19:02:55 -0500

    updated logout route

--- proj/config/routes.rb
+++ proj/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   	as :user do 
   		get 'login' => 'devise/sessions#new', as: :new_user_session
   		post 'login' => 'devise/sessions#create', as: :user_session
-  		get 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  		delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
   	end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

4a9028a8c38fcf252a8c37be118ef8d08bcc6079
Laurent <lmilne360@gmail.com>
2017-01-24 19:17:25 -0500

    Created Link resource

--- proj/db/schema.rb
+++ proj/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124215650) do
+ActiveRecord::Schema.define(version: 20170125000438) do
+
+  create_table "links", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_links_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

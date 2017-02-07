ce5d668099f23775b05662954f14cb26fdb0042c
lmilne360 <lmilne360@gmail.com>
2017-02-07 17:46:18 -0500

    added submit event to add comment form

--- proj/app/assets/javascripts/scripts.js
+++ proj/app/assets/javascripts/scripts.js
@@ -5,5 +5,29 @@ $(function() {
 
 
 function addComment() {
+  $('#new_comment').submit(function(e){
+    e.preventDefault();
+    console.log('It works')
+  });
+}
 
+
+
+
+
+
+function createLink() {
+    $('#new_link').submit(function(event) {
+        event.preventDefault();
+
+        var values = $(this).serialize();
+        var path = $(this).attr('action') // gets the action(path) of the form
+
+        var posting = $.post(path, values);
+
+        posting.done(function(data){
+          debugger
+        });
+
+    });
 }

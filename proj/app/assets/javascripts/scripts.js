7d73d9088122565bee2b7b91d1950914bba153f6
lmilne360 <lmilne360@gmail.com>
2017-02-07 18:35:58 -0500

    Serialized form values and submitted post request via jquery

--- proj/app/assets/javascripts/scripts.js
+++ proj/app/assets/javascripts/scripts.js
@@ -7,27 +7,9 @@ $(function() {
 function addComment() {
   $('#new_comment').submit(function(e){
     e.preventDefault();
-    console.log('It works')
+    var values = $(this).serialize();
+    var path = $(this).attr('action');
+    var posting = $.post(path, values);
+    //debugger
   });
-}
-
-
-
-
-
-
-function createLink() {
-    $('#new_link').submit(function(event) {
-        event.preventDefault();
-
-        var values = $(this).serialize();
-        var path = $(this).attr('action') // gets the action(path) of the form
-
-        var posting = $.post(path, values);
-
-        posting.done(function(data){
-          debugger
-        });
-
-    });
 }

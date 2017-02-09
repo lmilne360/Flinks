803b6933a71d92d32a526285762a54b2c5b86527
lmilne360 <lmilne360@gmail.com>
2017-02-09 03:38:34 -0500

    Made user links order by id

--- proj/app/controllers/users_controller.rb
+++ proj/app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@links = Link.where(user: @user)
+		@links = Link.where(user: @user).order('id DESC')
 	end
 end

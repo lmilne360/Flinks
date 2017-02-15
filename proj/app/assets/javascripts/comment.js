9e26984aad0758f129c623b0b7a0e796ed3586ca
Laurent Milne <lmilne360@gmail.com>
2017-02-15 16:13:46 -0500

    Reformated Comment prototype

--- proj/app/assets/javascripts/comment.js
+++ proj/app/assets/javascripts/comment.js
@@ -1,5 +1,19 @@
-function Comment(attributes) {
+class Comment {
+  constructor(attributes){
   this.body = attributes.body;
   this.user_id = attributes.user_id;
   this.commenter = attributes.commenter;
+}
+
+  render() {
+    var html = `<div class="comment">
+  		<p class="lead">${this.body}
+  			<small>Submitted by
+  				<a href="/users/${this.user_id}">${this.commenter}</a>
+  			</small>
+  		</p>
+  	</div>`;
+    return html;
+  }
+
 }

ed75d96e16f5419b9113eb038cd8f5d49d133898
Laurent Milne <lmilne360@gmail.com>
2017-02-17 03:34:10 -0500

    Changed method of obtaining next link id

--- proj/app/assets/javascripts/scripts.js
+++ proj/app/assets/javascripts/scripts.js
@@ -12,52 +12,62 @@ function addComment() {
         var posting = $.post("/comments.js", values);
         // get template
 
-        posting.done(function(data){
+        posting.done(function(data) {
 
-          comment = new Comment(data);
-          //Add the compiled html to pahe
-          $('#comments').append(comment.render());
+            comment = new Comment(data);
+            //Add the compiled html to pahe
+            $('#comments').append(comment.render());
 
-          //Increase comment counter
-          var size = parseInt($('#comment-size').html());
-          size ++;
-          $('#comment-size').html(size);
+            //Increase comment counter
+            var size = parseInt($('#comment-size').html());
+            size++;
+            $('#comment-size').html(size);
 
-          //reset form submit button
-          $('#new_comment input[type=submit]').attr('disabled', false);
+            //reset form submit button
+            $('#new_comment input[type=submit]').attr('disabled', false);
 
-          $('#comment_body').val('');
+            $('#comment_body').val('');
         });
     });
 }
 
 
 function toggleComments() {
-  $(document).on("click",'#comments-toggle', function(e){
-    e.preventDefault();
-    $("#comments").fadeToggle("slow");
-  });
+    $(document).on("click", '#comments-toggle', function(e) {
+        e.preventDefault();
+        $("#comments").fadeToggle("slow");
+    });
 }
 
 function toggleFave() {
-  $(document).on("click", ".link div.btn", function(e){
-    var item = $(this).parent().closest('div');
-    var id = $(this).attr('btn-id');
-    item.toggleClass('fave');
-    if (item.hasClass('fave')) {
-      localStorage.setItem('favourite' + id, 'fave');
-    } else {
-      localStorage.removeItem('favourite' + id);
-    }
+    $(document).on("click", ".link div.btn", function(e) {
+        var item = $(this).parent().closest('div');
+        var id = $(this).attr('btn-id');
+        item.toggleClass('fave');
+        if (item.hasClass('fave')) {
+            localStorage.setItem('favourite' + id, 'fave');
+        } else {
+            localStorage.removeItem('favourite' + id);
+        }
 
-  });
+    });
 }
 
 function nxtLink() {
+    //var all_ids = $('.linkIDs').data('ids');
     let id = $('#comment_link_id').val();
-    let nxtID = parseInt(id) + 1;
+    id = parseInt(id);
+    index = all_ids.indexOf(id);
+      if (index >= 0 && index < all_ids.length - 1) {
+        let nxtID = all_ids[index + 1];
+        appendNextLink(nxtID);
+      } else {
+        alert("That's all folks!");
+      }
+}
 
-    $.get(`/links/${nxtID}`, function(data) {
+function appendNextLink(id) {
+    $.get(`/links/${id}`, function(data) {
         data = $(data).find('.link').html();
 
         $('.link').empty().append(data);
